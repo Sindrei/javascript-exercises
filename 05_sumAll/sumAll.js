d4047d35d76674c023fd6f212bd204d957a24ec1
Sindrei <francoisdaniel.malan@gmail.com>
2022-09-27 18:07:45 +0200

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let c = 0;
+  let sum = 0;
 
+  if (
+    Number.isInteger(a) === false ||
+    Number.isInteger(b) === false ||
+    a < 0 ||
+    b < 0
+  ) {
+    return "ERROR";
+  } else {
+    if (a > b) {
+      c = b;
+      b = a;
+      a = c;
+    }
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
