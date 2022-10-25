6d6234c5c32edc3b18f88ab8695744feadd07034
Sindrei <francoisdaniel.malan@gmail.com>
2022-10-26 00:58:43 +0200

    Update 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,30 +1,5 @@
 const palindromes = function (word) {
-  const wordNoPunc = word
-    .toLowerCase()
-    .split("")
-    .filter((a) => {
-      switch (a) {
-        case "!":
-          return false;
-          break;
-        case "?":
-          return false;
-          break;
-        case ",":
-          return false;
-          break;
-        case ".":
-          return false;
-          break;
-        case " ":
-          return false;
-          break;
-        default:
-          return true;
-          break;
-      }
-    })
-    .join("");
+  const wordNoPunc = word.toLowerCase().replace(/[^a-z]/g, "");
   const wordReverse = wordNoPunc.split("").reverse().join("");
   return wordNoPunc === wordReverse;
 };
