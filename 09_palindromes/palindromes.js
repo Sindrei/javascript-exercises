8fcb0148c6687af588971637fea11d8dbb4450f3
Sindrei <francoisdaniel.malan@gmail.com>
2022-10-26 00:35:12 +0200

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const wordNoPunc = word
+    .toLowerCase()
+    .split("")
+    .filter((a) => {
+      switch (a) {
+        case "!":
+          return false;
+          break;
+        case "?":
+          return false;
+          break;
+        case ",":
+          return false;
+          break;
+        case ".":
+          return false;
+          break;
+        case " ":
+          return false;
+          break;
+        default:
+          return true;
+          break;
+      }
+    })
+    .join("");
+  const wordReverse = wordNoPunc.split("").reverse().join("");
+  return wordNoPunc === wordReverse;
 };
 
 // Do not edit below this line
