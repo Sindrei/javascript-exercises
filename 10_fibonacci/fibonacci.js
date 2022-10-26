d060347e8961e9e0b12a981cb4cab2545897ff82
Sindrei <francoisdaniel.malan@gmail.com>
2022-10-26 14:53:05 +0200

    Completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = (n) => {
+  Number(n);
+  if (n < 0) {
+    return "OOPS";
+  }
+  let a = 0;
+  let b = 1;
+  let c = 0;
+  for (let i = 1; i < n; i++) {
+    c = b;
+    b += a;
+    a = c;
+  }
+  return b;
 };
 
 // Do not edit below this line
