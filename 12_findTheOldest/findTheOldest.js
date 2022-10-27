b49a6e9a7331fd5bfa47a3d0e01f94b1e57c6ca9
Sindrei <francoisdaniel.malan@gmail.com>
2022-10-27 09:48:24 +0200

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = (a) => {
+  const oldestPerson = a.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentAge = getAge(
+      currentPerson.yearOfDeath,
+      currentPerson.yearOfBirth
+    );
+    return oldestAge > currentAge ? oldest : currentPerson;
+  });
+  return oldestPerson;
 };
+
+function getAge(death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
